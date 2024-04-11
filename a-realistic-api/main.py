b18e4267980dfcdb0c8e66997e2aa90efa70fab4
avnms <sarmaakondivnm@icloud.com>
2024-04-11 17:52:58 +1000

    created home page via templates and static files

--- a-realistic-api/main.py
+++ a-realistic-api/main.py
@@ -1,12 +1,17 @@
 import fastapi
 import uvicorn
+from starlette.templating import Jinja2Templates
+from starlette.requests import Request
+from starlette.staticfiles import StaticFiles
 
 app = fastapi.FastAPI()
+templates = Jinja2Templates("templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
-def index():
-    return "Hello Weather app!"
+def index(request: Request):
+    return templates.TemplateResponse("home/index.html", {"request": request})
 
 
 if __name__ == "__main__":
